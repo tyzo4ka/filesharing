4122777a7fa2cb7e944cb016e5d2d5f10c93ed91
tyzo4ka <tyzo4ka@gmail.com>
2020-03-14 10:14:46 +0600

    Add File model in webapp.models.py

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+ACCESS_CHOICES = (
+    ('0', 'Public'),
+    ('1', 'Hidden'),
+    ('2', 'Private'),
+)
+
+
+class File(models.Model):
+    author = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name="files",
+                               verbose_name="Автор")
+    file = models.FileField(upload_to='images', verbose_name="File")
+    caption = models.CharField(max_length=50, verbose_name="Caption")
+    access = models.CharField(max_length=20, choices=ACCESS_CHOICES, default=ACCESS_CHOICES[0][0],
+                              verbose_name='Access')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
+
+    def __str__(self):
+        return f'{self.author.username} {self.caption[:10]}'
